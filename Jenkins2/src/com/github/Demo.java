7e6addfab19824238f9e2c01de090edc9c63c166
Debkumar1234 <debkumarmallick2005@gmail.com>
2026-03-09 12:10:28 +0530

    update Demo.java

--- Jenkins2/src/com/github/Demo.java
+++ Jenkins2/src/com/github/Demo.java
@@ -4,6 +4,10 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println("Sum = "+sum);
 		System.out.println("Hello world");
 	}
 
